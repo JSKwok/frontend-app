0ec9cf155a2e9de81ca2c8bc79c16934f628b1ae
JSKwok <justin.s.kwok@gmail.com>
2019-08-17 19:38:30 -0400

    Added reactstrap to dependencies

--- src/pages/Exchange/Exchange.js
+++ src/pages/Exchange/Exchange.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { LeftSidebar } from './../../components';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 export default class Exchange extends Component {
   render() {
@@ -10,6 +11,10 @@ export default class Exchange extends Component {
       Exchange Page
         <div className="navigation">
           <LeftSidebar ref_code={this.props.location.state? this.props.location.state.ref_code : '21232'} />
+        </div>
+
+        <div className="exchange-investments">
+
         </div>
       </div>
     )
